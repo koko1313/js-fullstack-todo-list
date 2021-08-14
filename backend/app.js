7382d802510b193ca91e0064d7ae7278aafd297f
koko1313 <kaloyanvellichkov@gmail.com>
2021-08-14 14:02:05 +0300

    add notes

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 app.use(cors());
+app.use(bodyParser());
+
+// ##############################################################
+
+const notes = [];
 
 app.get("/", (req, res) => {
     res.send({ message: "Hello World!" });
 });
+
+app.post("/addNote", (req, res) => {
+    notes.push(req.body.note);
+    res.send({});
+});
+
+app.get("/getNotes", (req, res) => {
+    res.send({ notes: notes });
+});
+
+// ##############################################################
 
 app.listen(port, () => {
     console.log(`Server is running. Port: ${port}`);
--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,33 @@
-const request = (type, success) => {
+const request = (path, type, success, data) => {
     $.ajax({
-        url: "http://localhost:3000",
+        url: `http://localhost:3000/${path}`,
         type: type,
         dataType: "json",
+        data: JSON.stringify(data),
         headers: {
             "Access-Control-Allow-Origin": "*" // enable cross requests
         },
+        contentType: "application/json",
         success: success,
         error: () => {
             console.log("Something went wrong");
         }
     });
-}
+};
 
-const makeRequest = () => {
-    request("GET", (res) => {
-        document.getElementById("myDiv").innerHTML = res.message;
+const addNote = () => {
+    const note = document.getElementById("note").value;
+    request("addNote", "POST", getNotes, {
+        note: note
+    });
+};
+
+const getNotes = () => {
+    const results = document.getElementById("results");
+    results.innerHTML = "";
+    request("getNotes", "GET", (res) => {
+        for(const note of res.notes) {
+            results.innerHTML += `<li>${note}</li>`;
+        }
     });
 };
