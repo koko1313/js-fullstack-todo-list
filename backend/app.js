6a15b477d582aa033010098efa05ffb33836a43e
koko1313 <kaloyanvellichkov@gmail.com>
2021-08-14 14:11:10 +0300

    ES6 imports

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
 app.use(cors());
